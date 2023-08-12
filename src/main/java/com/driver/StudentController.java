e6b2c0eb6005c5b9c4007ada4248b57f4f0b331e
Hemanth-Kumar007 <skphemanth@gmail.com>
2023-08-12 23:05:35 +0530

    changes made

--- src/main/java/com/driver/StudentController.java
+++ src/main/java/com/driver/StudentController.java
@@ -1,5 +1,7 @@
 package com.driver;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,21 +21,29 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("students")
 public class StudentController {
 
+    HashMap<String, Student> studentDb = new HashMap<>();
+    HashMap<String, Teacher> teacherDb = new HashMap<>();
+    HashMap<String, List<String>> pairDb = new HashMap<>();
     @PostMapping("/add-student")
     public ResponseEntity<String> addStudent(@RequestBody Student student){
 
+        studentDb.put(student.getName(), student);
         return new ResponseEntity<>("New student added successfully", HttpStatus.CREATED);
     }
 
     @PostMapping("/add-teacher")
     public ResponseEntity<String> addTeacher(@RequestBody Teacher teacher){
 
+        teacherDb.put(teacher.getName(), teacher);
         return new ResponseEntity<>("New teacher added successfully", HttpStatus.CREATED);
     }
 
     @PutMapping("/add-student-teacher-pair")
     public ResponseEntity<String> addStudentTeacherPair(@RequestParam String student, @RequestParam String teacher){
 
+        List<String> list = pairDb.getOrDefault(teacher, new ArrayList<>());
+        list.add(student);
+        pairDb.put(teacher, list);
         return new ResponseEntity<>("New student-teacher pair added successfully", HttpStatus.CREATED);
     }
 
@@ -41,6 +51,9 @@ public class StudentController {
     public ResponseEntity<Student> getStudentByName(@PathVariable String name){
         Student student = null; // Assign student by calling service layer method
 
+        for(Student student1 : studentDb.values()){
+            if(student1.getName().equals(name)) student = student1;
+        }
         return new ResponseEntity<>(student, HttpStatus.CREATED);
     }
 
@@ -48,6 +61,9 @@ public class StudentController {
     public ResponseEntity<Teacher> getTeacherByName(@PathVariable String name){
         Teacher teacher = null; // Assign student by calling service layer method
 
+        for(Teacher teacher1 : teacherDb.values()){
+            if(teacher1.getName().equals(name)) teacher = teacher1;
+        }
         return new ResponseEntity<>(teacher, HttpStatus.CREATED);
     }
 
@@ -55,6 +71,9 @@ public class StudentController {
     public ResponseEntity<List<String>> getStudentsByTeacherName(@PathVariable String teacher){
         List<String> students = null; // Assign list of student by calling service layer method
 
+        for(String teacher1 : pairDb.keySet()){
+            if(teacher1.equals(teacher)) students = pairDb.get(teacher1);
+        }
         return new ResponseEntity<>(students, HttpStatus.CREATED);
     }
 
@@ -62,17 +81,32 @@ public class StudentController {
     public ResponseEntity<List<String>> getAllStudents(){
         List<String> students = null; // Assign list of student by calling service layer method
 
+        for(Student student : studentDb.values()){
+            students.add(student.getName());
+        }
         return new ResponseEntity<>(students, HttpStatus.CREATED);
     }
 
     @DeleteMapping("/delete-teacher-by-name")
     public ResponseEntity<String> deleteTeacherByName(@RequestParam String teacher){
 
+        if(teacherDb.containsKey(teacher)){
+            teacherDb.remove(teacher);
+        }
+        if(pairDb.containsKey(teacher)){
+            pairDb.remove(teacher);
+        }
         return new ResponseEntity<>(teacher + " removed successfully", HttpStatus.CREATED);
     }
     @DeleteMapping("/delete-all-teachers")
     public ResponseEntity<String> deleteAllTeachers(){
 
+        for(String teacher : teacherDb.keySet()){
+            teacherDb.remove(teacher);
+        }
+        for(String teacher : pairDb.keySet()){
+            pairDb.remove(teacher);
+        }
         return new ResponseEntity<>("All teachers deleted successfully", HttpStatus.CREATED);
     }
 }
